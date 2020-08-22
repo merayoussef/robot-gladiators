9179d74b003482e2c027cf1b4a937000d2bb7395
Mera Youssef <merayoussef91@hotmail.com>
2020-08-22 14:37:25 -0500

    activated battle, multiple plays

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,8 +16,6 @@ var enemyAttack = 12;
 var fight = function (enemyName) {
   // repeat and execute as long as the enemy robot is alive 
   while (enemyHealth > 0) {
-
-
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -67,5 +65,7 @@ var fight = function (enemyName) {
 }
 
 for (var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
